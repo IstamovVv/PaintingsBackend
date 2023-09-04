fc1f345e6116b283414ee93ad76aed445a3f308c
istamov.valeriy <istamov.valeriy@wildberries.work>
2023-09-04 22:46:54 +0300

    Products table

--- go.mod
+++ go.mod
@@ -11,6 +11,9 @@ require (
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
@@ -20,6 +23,7 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
---
+++ internal/repo/products.go
@@ -0,0 +1,103 @@
+package repo
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type StockType int
+
+const (
+	OnOrder StockType = iota
+	InStock
+	OutOfStock
+)
+
+type Product struct {
+	id              uint
+	name            string
+	images          []string
+	stock           StockType
+	discount        uint8
+	description     string
+	characteristics [][2]string
+}
+
+type ProductsTable struct {
+	db         *pgx.Conn
+	getAllStmt *pgconn.StatementDescription
+	insertStmt *pgconn.StatementDescription
+	deleteStmt *pgconn.StatementDescription
+}
+
+const (
+	getAllQuery = `SELECT * FROM products OFFSET $1 LIMIT $2`
+	insertQuery = `INSERT INTO products (name, images, stock, discount, description, characteristics) values ($1, $2, $3, $4, $5, $6)`
+	deleteQuery = `DELETE FROM products WHERE id = $1`
+)
+
+func NewProductsTable(db *pgx.Conn) (*ProductsTable, error) {
+	var (
+		err        error
+		getAllStmt *pgconn.StatementDescription
+		insertStmt *pgconn.StatementDescription
+		deleteStmt *pgconn.StatementDescription
+	)
+
+	getAllStmt, err = db.Prepare(context.Background(), "getAllProductsQuery", getAllQuery)
+	if err != nil {
+		return nil, err
+	}
+
+	insertStmt, err = db.Prepare(context.Background(), "insertProductQuery", insertQuery)
+	if err != nil {
+		return nil, err
+	}
+
+	deleteStmt, err = db.Prepare(context.Background(), "deleteProductQuery", deleteQuery)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ProductsTable{
+		db:         db,
+		getAllStmt: getAllStmt,
+		insertStmt: insertStmt,
+		deleteStmt: deleteStmt,
+	}, nil
+}
+
+func (t *ProductsTable) GetAllProducts(offset int, limit int) ([]Product, error) {
+	rows, err := t.db.Query(context.Background(), t.getAllStmt.Name, offset, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	var res []Product
+	for rows.Next() {
+		var (
+			p Product
+		)
+
+		err = rows.Scan(&p.id, &p.name, &p.images, &p.stock, &p.discount, &p.description, &p.characteristics)
+		if err != nil {
+			return nil, err
+		}
+
+		res = append(res, p)
+	}
+	rows.Close()
+
+	return res, rows.Err()
+}
+
+func (t *ProductsTable) Insert(p Product) error {
+	_, err := t.db.Exec(context.Background(), t.insertStmt.Name, p.name, p.images, p.stock, p.discount, p.description, p.characteristics)
+	return err
+}
+
+func (t *ProductsTable) Delete(id uint) error {
+	_, err := t.db.Exec(context.Background(), t.deleteStmt.Name, id)
+	return err
+}
